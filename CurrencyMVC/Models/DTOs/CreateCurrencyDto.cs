05eadc8aa3c8d871428111703a2fcb14c9e7591a
Baturhan Aykut <baturhanaykut@gmail.com>
2024-02-27 00:07:29 +0300

    All pages and controller were added

---
+++ CurrencyMVC/Models/DTOs/CreateCurrencyDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CurrencyMVC.Models.DTOs
+{
+    public class CreateCurrencyDto
+    {
+        [Required(ErrorMessage ="Döviz İsim Alanı Boş Geçilemez")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage ="Döviz Html İsim Alanı Boş Geçilemez")]
+        public string? AttributeName { get; set; }
+    }
+}
---
+++ CurrencyMVC/Models/VMs/ResultCurrencyVm.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyMVC.Models.VMs
+{
+    public class ResultCurrencyVm
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? AttributeName { get; set; }
+    }
+}
