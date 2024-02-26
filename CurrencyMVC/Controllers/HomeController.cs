501cbff4a12289ca58c3f20a672ec16a2d6eff59
Baturhan Aykut <baturhanaykut@gmail.com>
2024-02-26 22:01:21 +0300

    Add project files.

---
+++ CurrencyMVC/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
